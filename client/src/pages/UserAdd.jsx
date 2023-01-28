f692652777c230e7f0a807c35769c5e3e398d840
EdwinIvanS <sedwin118@hotmail.com>
2023-01-28 18:04:06 -0500

    agregando mensaje emergente para el registro de un usuario

--- client/src/pages/UserAdd.jsx
+++ client/src/pages/UserAdd.jsx
@@ -1,6 +1,90 @@
-import { TextField, Box, Stack, Button, Card } from "@mui/material";
+import {
+  TextField,
+  Box,
+  Stack,
+  Button,
+  Card,
+  Alert
+} from "@mui/material";
+import React, { useState } from "react";
 
 export default function UserAdd() {
+  const [name, setName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const [errorName, setErrorName] = useState(false);
+  const [errorLastName, setErrorLastName] = useState(false);
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
+  const [errorPasswordConfirm, setErrorPasswordConfirm] = useState(false);
+
+  const [msnErrorName, setMsnErrorName] = useState('');
+  const[msnErrorLastName, setMsnErrorLastName] = useState("");
+  const [msnErrorEmail, setMsnErrorEmail] = useState("");
+  const [msnErrorPassword, setMsnErrorPassword] = useState("");
+  const [msnErrorPasswordC, setMsnErrorPasswordC] = useState("");
+
+  const [alert, setAlert] = useState(false);
+
+  const handleOnSubmit = (e) => {  
+    if(name===''){
+      setErrorName(true); 
+      setMsnErrorName("Campo obligatorio");
+    }else{
+      setErrorName(false);
+      setMsnErrorName("");
+    }
+
+    if (lastName === "") {
+      setErrorLastName(true);
+      setMsnErrorLastName("Campo obligatorio");
+    } else {
+      setErrorLastName(false);
+      setMsnErrorLastName("");
+    }
+
+    if (email === "") {
+      setErrorEmail(true);
+      setMsnErrorEmail("Campo obligatorio");
+    } else {
+      setErrorEmail(false);
+      setMsnErrorEmail("");
+    }
+
+    if (password === "") {
+      setErrorPassword(true);
+      setMsnErrorPassword("Campo obligatorio");
+    } else {
+      setErrorPassword(false)
+      setMsnErrorPassword("");
+    }
+
+    if (passwordConfirm === "") {
+      setErrorPasswordConfirm(true);
+      setMsnErrorPasswordC("Campo obligatorio");      
+    } else {
+      if (passwordConfirm !== password) {
+        setErrorPasswordConfirm(true);
+        setMsnErrorPasswordC("No coincide el password");
+      }
+      else{
+        setErrorPasswordConfirm(false);
+        setMsnErrorPasswordC("");
+      }      
+    }
+
+    if(name!=='' && lastName !== "" && email !== "" && password !== "" 
+                && passwordConfirm !== "" && passwordConfirm === password) {
+      setAlert(true);
+    }
+    else{
+      e.preventDefault();
+    }
+  };
+
   return (
     <div className="userAddContainer">
       <Card className="userAddCard">
@@ -8,49 +92,83 @@ export default function UserAdd() {
           <h1>Register</h1>
           <p>Create your account. It's free and only takes a minute.</p>
         </div>
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "26ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField required id="outlined-required" label="First Name" />
-          <TextField required id="outlined-required" label="Last Name" />
-        </Box>
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 2, width: "54ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <div>
-            <TextField required id="outlined-required" label="Email" />
+        <form onSubmit={handleOnSubmit}>
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "26ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
             <TextField
               required
+              error={errorName}
+              id="outlined-required"
+              label="First Name"
+              onChange={(e) => setName(e.target.value)}
+              helperText={msnErrorName}
+            />
+            <TextField
+              required
+              error={errorLastName}
+              id="outlined-required"
+              label="Last Name"
+              onChange={(e) => setlastName(e.target.value)}
+              helperText={msnErrorLastName}
+            />
+          </Box>
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 2, width: "54ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              required
+              error={errorEmail}
+              id="outlined-required"
+              label="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              helperText={msnErrorEmail}
+            />
+
+            <TextField
+              required
+              error={errorPassword}
               id="outlined-password-input"
               label="Password"
               type="password"
               autoComplete="current-password"
+              onChange={(e) => setPassword(e.target.value)}
+              helperText={msnErrorPassword}
             />
+
             <TextField
               required
+              error={errorPasswordConfirm}
               id="outlined-password-input"
               label="Confirm Password"
               type="password"
               autoComplete="current-password"
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+              helperText={msnErrorPasswordC}
             />
-          </div>
-        </Box>
-        <Stack className="conteainedButton" direction="row" spacing={0}>
-          <Button variant="contained" size="large">
-            Crear
-          </Button>
-        </Stack>
+          </Box>
+          <Stack className="conteainedButton" direction="row" spacing={0}>
+            <Button type="submit" variant="contained" size="large">
+              Crear
+            </Button>
+          </Stack>
+        </form>
       </Card>
+      {alert && (
+        <Alert onClose={() => {}}>
+          Register success!
+        </Alert>
+      )}
     </div>
   );
 }
